3a35c4f2efad2da83e5e354fc58c64acbd4dae3d
va-deem <vadim.stebakov@gmail.com>
2022-10-29 12:18:24 +0300

    Add authentication using Github provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 // import '../styles/normalize.scss';
 import '../styles/global.scss';
 import '../styles/main.scss';
-import '../styles/admin.scss';
 import { SessionProvider } from 'next-auth/react';
 
 import 'highlight.js/styles/github.css';
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
--- pages/admin/posts/new.tsx
+++ pages/admin/posts/new.tsx
@@ -4,9 +4,15 @@ import PostForm from '../../../components/admin/_post-form';
 import { useRouter } from 'next/router';
 import convertToHtml from '../../../lib/mdToHtml';
 import { createPost } from '../../../services/blogService';
+import { useSession } from 'next-auth/react';
 
 const CreatePost = () => {
   const router = useRouter();
+  const { data: session, status } = useSession();
+
+  if (!session) {
+    return <div>{`Your status is: ${status}`}</div>;
+  }
 
   const handlePostCreate = async (formValues) => {
     formValues.contentHtml = convertToHtml(formValues.content);
